00ab4ab5a0d94922c1cc30c9c8106a1e8f2a81ac
Andrew J. Ogren <andrew.j.ogren.18@dartmouth.edu>
2017-04-30 16:29:11 -0400

    added modal for login

--- app.js
+++ app.js
@@ -1,3 +1,86 @@
+
+var nav = new Vue({
+  el: '#fake-nav',
+  methods: {
+    open: function(which, e) {
+      e.preventDefault();
+      modal.active = which;
+    },
+  }
+});
+
+var modal_submit_register = 'Register';
+var modal_submit_password = 'Reset Password';
+var modal_submit_login = 'Login';
+
+var modal = new Vue({
+  el: '#login-modal',
+  data: {
+    active: null,
+    submitted: null,
+
+    // Submit button text
+    registerSubmit: modal_submit_register,
+    passwordSubmit: modal_submit_password,
+    loginSubmit: modal_submit_login,
+
+    // Modal text fields
+    registerName: '',
+    registerEmail: '',
+    registerPassword: '',
+    loginUser: '',
+    loginPassword: '',
+    passwordEmail: '',
+
+    // Modal error messages
+    registerError: '',
+    loginError: '',
+    passwordError: '',
+  },
+  methods: {
+    close: function(e) {
+      e.preventDefault();
+      if (e.target === this.$el) {
+        this.active = null;
+      }
+    },
+    flip: function(which, e) {
+      e.preventDefault();
+      if (which !== this.active) {
+        this.active = which;
+      }
+    },
+    submit: function(which, e) {
+      e.preventDefault();
+      this.submitted = which
+      var data = {
+        form: which
+      };
+
+      switch (which) {
+        case 'register':
+          data.name = this.registerName;
+          data.email = this.registerEmail;
+          data.password = this.registerPassword;
+          this.$set('registerSubmit', 'Registering...');
+          break;
+        case 'login':
+          data.user = this.loginUser;
+          data.password = this.loginPassword;
+          this.$set('loginSubmit', 'Logging In...');
+          break;
+        case 'password':
+          data.email = this.passwordEmail;
+          this.$set('passwordSubmit', 'Resetting Password...')
+          break;
+      }
+
+      // TODO: submit our `data` variable
+    }
+  }
+});
+
+
 new Vue({
 
   // We want to target the div with an id of 'events'
@@ -50,7 +133,7 @@ new Vue({
       //   this.events.push(events[i]);
       // }
     },
-  
+
     // Adds an event to the existing events array
     addEvent: function() {
       if(this.event.name) {
@@ -62,7 +145,7 @@ new Vue({
     deleteEvent: function(index) {
       if(confirm("Are you sure you want to delete this event?")) {
         // $remove is a Vue convenience method similar to splice
-        this.events.splice(index, 1);        
+        this.events.splice(index, 1);
       }
     }
   }
