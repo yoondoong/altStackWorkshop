da6851165ced786dfd8ce7927bf6fa0f188c99d0
Eun Kyung Yoon <eun.kyung.yoon.19@dartmouth.edu>
2017-05-04 15:24:20 -0400

    workshop

--- app.js
+++ app.js
@@ -56,4 +56,63 @@ var modal = new Vue({
   }
 });
 
-// ADD CODE HERE TO CREATE A VUE INSTANCE
+var eventsPage = new Vue({
+
+  // We want to target the div with an id of 'events'
+  el: '#events',
+
+  // Here we can register any values or collections that hold data
+  // for the application
+  data: {
+    event: { user: '', name: '', description: '', date: '' },
+    events: []
+  },
+
+  // Anything within the mounted function will run when the application loads
+  mounted: function() {
+    // call fetchEvents here!
+    this.fetchEvents();
+  },
+
+  // Methods we want to use in our application are registered here
+  methods: {
+    fetchEvents: function() {
+      let events = '';
+      let arr = [];
+      $.get("https://hidden-retreat-66994.herokuapp.com/AllEvents", (data) => {
+        arr = JSON.parse(data);
+        this.events =  arr;
+      });
+    },
+    addEvent: function() {
+      if(this.event.name) {
+        this.event.user = user;
+        this.events.push(this.event);
+
+        $.post("https://hidden-retreat-66994.herokuapp.com/PutEvent", {
+          name: this.event.name,
+          user: this.event.user,
+          description: this.event.description,
+          date: this.event.date,
+        }).done( function(data) {
+          window.location.reload();
+        });
+
+        this.event = { name: '', user: '', description: '', date: '' };
+      }
+    },
+    deleteEvent: function(index) {
+      if(confirm("Are you sure you want to delete this event?")) {
+        $.post("https://hidden-retreat-66994.herokuapp.com/DeleteEvent", {
+          name: this.events[index].name,
+          user: this.events[index].user,
+          description: this.events[index].description,
+          date: this.events[index].date,
+        }).done( function(data) {
+          window.location.reload();
+        });
+      }
+    },
+    // add fetchEvents, addEvent, and deleteEvent
+  }
+});
